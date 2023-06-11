8110594cbc5dc4e1de498c978b936aacc9928777
Guilherme Moura <gaxco123@gmail.com>
2023-06-11 15:29:14 -0300

    Primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+/*!
+ * Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
